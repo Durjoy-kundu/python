995be7c9991de7c114797c5cfd6208cb2307445d
Durjoy kundu <durjoykundu07@gmail.com>
2022-02-13 00:45:52 +0600

    Printing programm in python

---
+++ chapter1Practice.py
@@ -0,0 +1,7 @@
+#print the poem -> twinkle twinkle little star
+'''printing multiple line in python'''
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
---
+++ first.py
@@ -0,0 +1,7 @@
+#comment
+"""
+multi 
+line 
+comment
+"""
+print("hello world")
